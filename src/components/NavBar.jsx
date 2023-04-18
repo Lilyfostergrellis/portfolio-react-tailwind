a464704516f46c5f5a2029fe70089aa792cd4bfc
Lilyfostergrellis <lilyfostergrellis2@gmail.com>
2023-04-18 13:28:20 +0100

    icon for dropdown, navLinks in .map for clearer code/easier to modify

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,6 +2,26 @@ import React from 'react';
 import {FaBars, FaTimes} from 'react-icons/fa';
 
 const NavBar = () => {
+
+    const navLinks = [
+        {
+            id: 1,
+            link: "home",
+        },
+        {
+            id: 2,
+            link: "about",
+        },
+        {
+            id: 3,
+            link: "portfolio",
+        },
+        {
+            id: 4,
+            link: "contact",
+        },
+    ]; {/*created an array for each link in the navbar, to be able to loop over and give unified attributes and behaviours. Any change to names or additional tabs to be made here*/}
+
   return (
     <div className="flex justify-between items-center w-full h-20 px-4 text-white bg-black fixed">
     {/*display-flex, justify-content space-between, align-items center, width 100%, white text black background, position fixed*/}
@@ -10,12 +30,20 @@ const NavBar = () => {
             {/*text is 36px, margin-left 8px*/}
         </div>
 
-        <ul className="flex">
-            <li className="px-4 cursor-pointer">Home</li>
-            <li className="px-4 cursor-pointer">Home</li>
-            <li className="px-4 cursor-pointer">Home</li>
+        <ul className="hidden md:flex">
+            {navLinks.map(({ id, link}) => (
+            <li
+                key={link}
+                className="px-4 cursor-pointer capitalize font-medium text-gray-400 hover:scale-105 duration-200"
+            >
+                {link}
+            </li>
+            ))}
         </ul>
 
+        <div className='cursor-pointer pr-4 z-10 text-gray-200'>
+            <FaBars size={40}/>
+        </div>
 
     </div>
   )
