81b39cc36cfe450adced4c3e58ba3e4ac0033920
Lilyfostergrellis <lilyfostergrellis2@gmail.com>
2023-04-18 17:11:46 +0100

    components struture, all files and route paths linked to app.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
-import NavBar from "./components/NavBar";
+import About from "./components/About";
+import Contact from "./components/Contact";
 import Home from "./components/Home";
+import NavBar from "./components/NavBar";
+import Portfolio from "./components/Portfolio";
+import Social from "./components/Social";
 
 function App() {
   return (
     <div>
       <NavBar />
       <Home />
+      <About />
+      <Portfolio />
+      <Contact />
+
+      <Social />
     </div>
   );
 }
