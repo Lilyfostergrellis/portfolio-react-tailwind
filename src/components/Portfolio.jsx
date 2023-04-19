a0408006e98d851eb1baf6deda644950700d2c62
Lilyfostergrellis <lilyfostergrellis2@gmail.com>
2023-04-19 09:42:03 +0100

    projects added to page, contact form also

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,9 +1,69 @@
-import React from 'react'
+import React from "react";
+import weatherForecast from "../assets/weatherForecast.png";
+import musicPlatform from "../assets/musicPlatform.png";
+import snakeGame from "../assets/snakeGame.png";
+import multiplicationGame from "../assets/multiplicationGame.png";
+import digitalClock from "../assets/digitalClock.png"
 
 const Portfolio = () => {
-  return (
-    <div>Portfolio</div>
-  )
-}
+  const portfolios = [
+    {
+      id: 1,
+      src: weatherForecast,
+    },
+    {
+      id: 2,
+      src: musicPlatform,
+    },
+    {
+      id: 3,
+      src: snakeGame,
+    },
+    {
+      id: 4,
+      src: multiplicationGame,
+    },
+    {
+      id: 5,
+      src: digitalClock,
+    },
+  ];
 
-export default Portfolio
+  return (
+    <div
+      name="portfolio"
+      className="bg-gradient-to-b from-black to-gray-800 w-full text-white md:h-screen"
+    >
+      <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
+        <div className="pb-8">
+          <p className="text-4xl font-bold inline border-b-4 border-gray-500">
+            Portfolio
+          </p>
+          <p className="py-6">Check out some of my work right here</p>
+        </div>
+
+        <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-8 px-12 sm:px-0">
+          {portfolios.map(({ id, src }) => (
+            <div key={id} className="shadow-md shadow-gray-600 rounded-lg">
+              <img
+                src={src}
+                alt=""
+                className="rounded-md duration-200 hover:scale-105"
+              />
+              <div className="flex items-center justify-center">
+                <button className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105">
+                  Demo
+                </button>
+                <button className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105">
+                  Code
+                </button>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Portfolio;
